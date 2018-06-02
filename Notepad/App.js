fd232fa518c2950aa8fa41a6f8ed9264a5d77c6f
Justin Wu <justin.wu@mail.utoronto.ca>
2018-06-02 16:24:42 -0400

    Broke something

--- Notepad/App.js
+++ Notepad/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, Alert, TouchableOpacity, View, FlatList, Button } from 'react-native';
 import { stackManager, createStackNavigator } from 'react-navigation'
 
 class HomeScreen extends React.Component {
@@ -7,17 +7,20 @@ class HomeScreen extends React.Component {
     title: 'Notes',
   }
 
-
   render() {
     var notes = loadNotes();
 
     return (
       <View style={styles.container}>
-        //<Text>{notes.list}</Text>
         <FlatList
-          data={notes}
-
-          renderItem={({item}) => <ListItem title={item.title} text={item.text} date={item.date}/>}
+          data={notes.notes}
+          renderItem={({item}) =>
+          <ListItem
+            title={item.key}
+            text={item.text}
+            date={item.date}
+            navigation={this.props.navigation}
+          />}
         />
       </View>
     )
@@ -26,27 +29,71 @@ class HomeScreen extends React.Component {
 
 class ListItem extends React.Component {
   constructor(props){
-      super(props)
+    super(props)
 
-      this.state={
-        title: this.props.title,
-        date: this.props.date,
-        text: this.props.text,
-      }
+    this.state={
+      title: this.props.title,
+      date: this.props.date,
+      text: this.props.text,
+    }
   }
 
   render() {
+    const { navigate } = this.props.navigation
     return(
-      <View style={styles.listItem}>
-        <View style={{flexDirection: 'row'}}>
-          <View>
+      <TouchableOpacity style={styles.listItem} onPress={() =>
+          navigate('Note', { title: this.state.title, text: this.state.text })
+        }>
+        <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+          <View style={{flex: 1}}>
             <View style={{flex: 1, flexDirection: 'row', alignItems: 'baseline'}}>
               <Text style={styles.headerText}>{this.state.title}</Text>
               <Text numberOfLines={1} style={styles.paragraphText}>{" - " + this.state.date}</Text>
             </View>
           </View>
+          <Button
+            title="Delete"
+            style={{alignSelf: 'flex-end'}}
+            onPress={() => {
+              Alert.alert(
+                'Delete',
+                'Are you sure you want to delete this note?',
+                [
+                  {text: 'Yes', onPress: () => console.log('Yes Pressed')},
+                  {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                ]
+              )
+            }}
+            />
         </View>
         <Text numberOfLines={1} style={styles.paragraphText}>{this.state.text}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+class NoteScreen extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state={
+      title: this.props.navigation.state.params.title,
+      text: this.props.navigation.state.params.text,
+    }
+  }
+  static navigationOptions = {
+    title: "Note"
+  }
+
+  render() {
+
+    return(
+      <View style={styles.noteScreen}>
+        <TextInput
+          editable = {true}
+          multiline = {true}
+          defaultValue = {this.state.text}
+        />
       </View>
     )
   }
@@ -55,6 +102,7 @@ class ListItem extends React.Component {
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
+    Note: NoteScreen,
   },
   {
     initialRouteName: 'Home'
@@ -62,12 +110,7 @@ const RootStack = createStackNavigator(
 );
 
 function loadNotes() {
-  //return require('./notes.json')
-  return {list: [
-    "title": "Some title 1",
-    "date": "05/23/18",
-    "text": "This is a paragraph with some shit in it"
-  ]};
+  return require('./notes.json')
 }
 
 export default class App extends React.Component {
@@ -77,23 +120,30 @@ export default class App extends React.Component {
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#eee',
-    alignItems: 'flex-start',
     justifyContent: 'center',
     paddingLeft: 20,
     paddingRight: 20,
   },
   listItem: {
-    backgroundColor: '#eee',
     flex: 1,
-    alignItems: 'flex-start',
     justifyContent: 'space-between',
-    paddingVertical: 8,
+    paddingVertical: 12,
     borderBottomColor: '#999',
     borderBottomWidth: 1,
+    alignSelf: 'stretch',
+  },
+  noteScreen: {
+    flex: 1,
+    backgroundColor: '#eee',
+    justifyContent: 'flex-start',
+    paddingVertical: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
   headerText: {
     //color: '#333',
